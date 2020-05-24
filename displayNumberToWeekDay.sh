fc6c6c44e868d7b2b7a9bfcca610230495268a69
unknown <neerajkumar8546@gmail.com>
2020-05-24 16:01:51 +0530

    Display number to week day

---
+++ displayNumberToWeekDay.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+read -p "Enter Single Digit Number : " x
+
+case "$x" in
+   "1") echo "Sunday" 
+   ;;
+   "2") echo "Monday" 
+   ;;
+   "3") echo "Tuesday" 
+   ;;
+	"4") echo "Wednesday"
+   ;;
+	"5") echo "Thursday"
+   ;;
+	"6") echo "Friday"
+   ;;
+	"7") echo "Saturday"
+   ;;
+	*) echo "Invalid Input"
+   ;;
+
+esac
