80424388a40b3b0a85f4a3eccdf6b56c3a981c73
unknown <neerajkumar8546@gmail.com>
2020-05-24 13:45:45 +0530

    convert number to word using case

---
+++ con_single_digit_num_To_word_usingCase.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+read -p "Enter Single Digit Number : " a
+case "$a" in
+   "0") echo "Zero" 
+   ;;
+   "1") echo "One" 
+   ;;
+   "2") echo "Two" 
+   ;;
+	"3") echo "Three"
+   ;;
+	"4") echo "Four"
+   ;;
+	"5") echo "Five"
+   ;;
+	"6") echo "Six"
+   ;;
+	"7") echo "Seven"
+   ;;
+	"8") echo "Eight"
+   ;;
+	"9") echo "Nine"
+   ;;
+	*) echo "Enter Valid Number Single Digit"
+   ;;
+
+
+esac
